47c1614be4c363aaa1d15c3fe0e98e35346ddb66
Mike Glenn <mglenn@truefitsolutions.com>
2011-05-09 23:00:38 -0400

    test 2

--- HookSpike/Program.cs
+++ HookSpike/Program.cs
@@ -1,16 +1,21 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace HookSpike {
-  class Program {
-    static void Main(string[] args) {
-
+  internal class Program {
+    private static void Main(string[] args) {
       var arguments = new List<string>(args);
 
       foreach(var argument in arguments) {
         Console.WriteLine("{0,2}: {1}", arguments.IndexOf(argument), argument);
+      }
+
+      IDictionary environmentVariables = Environment.GetEnvironmentVariables();
+      foreach(DictionaryEntry de in environmentVariables) {
+        Console.WriteLine("  {0} = {1}", de.Key, de.Value);
       }
     }
   }
