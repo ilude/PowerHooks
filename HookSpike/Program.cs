64c2c04e7aad194f5893069c8f842c403acdc905
Mike Glenn <mglenn@truefitsolutions.com>
2011-05-09 22:44:51 -0400

    test

---
+++ HookSpike/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HookSpike {
+  class Program {
+    static void Main(string[] args) {
+
+      var arguments = new List<string>(args);
+
+      foreach(var argument in arguments) {
+        Console.WriteLine("{0,2}: {1}", arguments.IndexOf(argument), argument);
+      }
+    }
+  }
+}
