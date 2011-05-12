82ee9c18c9cb7d864681d278ad20549c3f2a8107
Mike Glenn <mglenn@truefitsolutions.com>
2011-05-12 00:47:51 -0400

    working ps call

--- HookSpike/Program.cs
+++ HookSpike/Program.cs
@@ -1,27 +1,63 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
 using System.Text;
 
 namespace HookSpike {
   internal class Program {
     private static void Main(string[] args) {
-      var arguments = new List<string>(args);
+      var runspace = RunspaceFactory.CreateRunspace();
 
-      foreach(var argument in arguments) {
-        Console.WriteLine("{0,2}: {1}", arguments.IndexOf(argument), argument);
-      }
-      
-      var env = Environment.GetEnvironmentVariables().Cast<DictionaryEntry>().ToDictionary(de => de.Key.ToString(), de => de.Value.ToString()).OrderBy(key => key.Key);
-      foreach(var pair in env) {
-        Console.WriteLine("  {0} = {1}", pair.Key, pair.Value);
+      // open it
+
+      runspace.Open();
+
+      // create a pipeline and feed it the script text
+
+      var pipeline = runspace.CreatePipeline();
+      pipeline.Commands.AddScript("ps | Out-String");
+
+       // execute the script
+
+      var results = pipeline.Invoke();
+
+      // close the runspace
+
+      runspace.Close();
+
+      // convert the script result into a single string
+
+      var stringBuilder = new StringBuilder();
+      foreach (var obj in results)
+      {
+          stringBuilder.AppendLine(obj.ToString());
       }
 
-      Console.WriteLine(Directory.GetCurrentDirectory());
-      
-      Environment.Exit(1);
+      Console.WriteLine(stringBuilder.ToString());
+
+      Console.ReadLine();
+
+      //var arguments = new List<string>(args);
+
+
+
+      //foreach(var argument in arguments) {
+      //  Console.WriteLine("{0,2}: {1}", arguments.IndexOf(argument), argument);
+      //}
+
+      //var env = Environment.GetEnvironmentVariables().Cast<DictionaryEntry>().ToDictionary(de => de.Key.ToString(), de => de.Value.ToString()).OrderBy(key => key.Key);
+      //foreach(var pair in env) {
+      //  Console.WriteLine("  {0} = {1}", pair.Key, pair.Value);
+      //}
+
+      //Console.WriteLine(Directory.GetCurrentDirectory());
+
+      //Environment.Exit(1);
     }
   }
 }
